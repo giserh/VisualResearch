76d095767083f8c40d7ce28e48a259d7b352ea33
Wotori Movako <prostosobolev@gmail.com>
2018-11-29 14:52:58 +0300

    bug fix

--- toList.py
+++ toList.py
@@ -1,24 +1,24 @@
 def txtToDict(fileName):
     with open ('creatures.txt') as data:
         dataRaw = data.readlines()
-
     #Data to Dict
     dataDict = {}
     dataListTemp=[]
     for i in range(len(dataRaw)):
         dataListTemp.append(dataRaw[i][:-1])
         if dataRaw[i] == '\n' or '':
+            #first = key second = value
             dataDict.update({dataListTemp[0]:[dataListTemp[1]]})
+            #start work from third to end of dict list
             for i in range(len(dataListTemp[2:])):
                 dataDict[dataListTemp[0]].append(dataListTemp[2:][i])
             dataDict[dataListTemp[0]].pop()
             dataListTemp=[]
     return dataDict
 
-def createFolderStructure(folderName):
-'''create folders with value name inside each key 
-// automatically creates KEYS folders!'''
+def createFolderStructure():
+#create folders with value name inside each key automatically creates KEYS folders!
     for key in list(data.keys()):
         for value in data[key]:
-            path = (folderName + '/{}/'.format(key))
+            path = ('structure/{}/'.format(key))
             os.makedirs(path + value)
